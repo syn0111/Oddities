b9df9ba32095a4a58e018b8191d782a7f9651e14
syn#0111 <115029700+syn0111@users.noreply.github.com>
2023-03-15 17:46:44 +0800

    Update bot.js

--- Source/bot.js
+++ Source/bot.js
@@ -8,14 +8,9 @@ const { loadVariables } = require('./Handlers/variables')
 const client = new Client({
     intents: [Object.keys(GatewayIntentBits)],
     partials: [Object.keys(Partials)],
-    allowedMentions: {
-        parse: ["roles", "users", "everyone"],
-        repliedUser: false,
-      },
 })
 
 client.commands = new Collection();
-client.devcommands = new Collection();
 
 process.on("unhandledRejection", async (err, promise) => {
     console.error(`[ANTI-CRASH] Unhandled Rejection: ${err}`);
